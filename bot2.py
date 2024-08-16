9200fa42a522f0b9718012348efb85ce25aa065e
romanvolkonidov <romanvolkonidov@gmail.com>
2024-08-17 02:08:35 +0300

    Update bot2.py

--- bot2.py
+++ bot2.py
@@ -1,139 +1,117 @@
 import os
 from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import Application, CommandHandler, CallbackQueryHandler, ContextTypes
-import asyncio
+
 
 
 TOKEN = os.getenv('BOT_TOKEN', '7152066894:AAGkTh2QLFNMSF7Z5dJdfj7IDjcDcDPoKnM')
+
+
 
 
 questions = [
     {
         "question": "1. Какой элемент HTML используется для добавления заголовка страницы? 📑",
         "options": ["<header>", "<title>", "<h1>", "<meta>"],
-        "correct_option_id": 1,
-        "explanation": "Тег <title> используется для определения заголовка страницы, который отображается на вкладке браузера."
+        "correct_option_id": 1
     },
     {
         "question": "2. Какой элемент HTML используется для создания гиперссылок? 🔗",
         "options": ["<a>", "<link>", "<button>", "<url>"],
-        "correct_option_id": 0,
-        "explanation": "Тег <a> создает гиперссылки, которые могут ссылаться на другие страницы или ресурсы."
+        "correct_option_id": 0
     },
     {
         "question": "3. Какой атрибут HTML используется для открытия ссылки в новой вкладке? 🆕",
         "options": ["target=\"_self\"", "target=\"_top\"", "target=\"_blank\"", "target=\"_parent\""],
-        "correct_option_id": 2,
-        "explanation": "Атрибут target=\"_blank\" открывает ссылку в новой вкладке или окне браузера."
+        "correct_option_id": 2
     },
     {
         "question": "4. Какой элемент HTML используется для вставки изображения? 🖼",
         "options": ["<img>", "<picture>", "<photo>", "<image>"],
-        "correct_option_id": 0,
-        "explanation": "Тег <img> используется для вставки изображений на веб-страницу."
+        "correct_option_id": 0
     },
     {
         "question": "5. Что указывает атрибут alt в теге <img>? 📝",
         "options": ["Размер изображения", "URL изображения", "Альтернативный текст для изображения", "Выравнивание изображения"],
-        "correct_option_id": 2,
-        "explanation": "Атрибут alt задает альтернативный текст, который отображается, если изображение не может быть загружено."
+        "correct_option_id": 2
     },
     {
         "question": "6. Какой элемент HTML используется для создания списка с маркерами? 📋",
         "options": ["<ul>", "<ol>", "<list>", "<item>"],
-        "correct_option_id": 0,
-        "explanation": "Тег <ul> используется для создания ненумерованных списков с маркерами."
+        "correct_option_id": 0
     },
     {
         "question": "7. Какой элемент HTML используется для создания списка с нумерацией? 🔢",
         "options": ["<ol>", "<ul>", "<list>", "<item>"],
-        "correct_option_id": 0,
-        "explanation": "Тег <ol> используется для создания упорядоченных списков с нумерацией."
+        "correct_option_id": 0
     },
     {
         "question": "8. Какой элемент HTML используется для добавления текста под изображением? 📝",
         "options": ["<figcaption>", "<caption>", "<text>", "<description>"],
-        "correct_option_id": 0,
-        "explanation": "Тег <figcaption> добавляет описание или подпись к изображению."
+        "correct_option_id": 0
     },
     {
         "question": "9. Какой элемент HTML используется для создания раздела на странице? 🗂",
         "options": ["<section>", "<div>", "<article>", "<header>"],
-        "correct_option_id": 0,
-        "explanation": "Тег <section> используется для группировки связанных элементов и разделов на странице."
+        "correct_option_id": 0
     },
     {
         "question": "10. Какой элемент HTML используется для горизонтальной линии? 🌐",
         "options": ["<hr>", "<line>", "<divider>", "<break>"],
-        "correct_option_id": 0,
-        "explanation": "Тег <hr> создает горизонтальную линию, которая часто используется для разделения контента."
+        "correct_option_id": 0
     },
     {
         "question": "11. Какое CSS-свойство задает цвет фона элемента? 🌈",
         "options": ["color", "background-color", "border-color", "text-color"],
-        "correct_option_id": 1,
-        "explanation": "Свойство background-color задает цвет фона элемента."
+        "correct_option_id": 1
     },
     {
         "question": "12. Какое CSS-свойство используется для установки шрифта текста? 🔠",
         "options": ["font-size", "font-family", "text-align", "line-height"],
-        "correct_option_id": 1,
-        "explanation": "Свойство font-family устанавливает шрифт, используемый для текста."
+        "correct_option_id": 1
     },
     {
         "question": "13. Какое CSS-свойство используется для задания максимальной ширины элемента? 📏",
         "options": ["width", "max-height", "max-width", "height"],
-        "correct_option_id": 2,
-        "explanation": "Свойство max-width задает максимальную ширину элемента, которая не будет превышена."
+        "correct_option_id": 2
     },
     {
         "question": "14. Какое CSS-свойство задает внутренние отступы элемента? ⛓",
         "options": ["margin", "padding", "border", "spacing"],
-        "correct_option_id": 1,
-        "explanation": "Свойство padding устанавливает внутренние отступы внутри элемента, создавая пространство между содержимым и границей."
+        "correct_option_id": 1
     },
     {
         "question": "15. Какое CSS-свойство управляет выравниванием текста? 📝",
         "options": ["text-decoration", "text-align", "font-style", "vertical-align"],
-        "correct_option_id": 1,
-        "explanation": "Свойство text-align управляет выравниванием текста по левому, правому краю или по центру."
+        "correct_option_id": 1
     },
     {
         "question": "16. Какое CSS-свойство используется для установки стиля границы элемента? 🖊",
         "options": ["border", "border-width", "border-color", "border-radius"],
-        "correct_option_id": 0,
-        "explanation": "Свойство border задает стиль, ширину и цвет границы элемента."
+        "correct_option_id": 0
     },
     {
         "question": "17. Какое CSS-свойство задает размер шрифта текста? 📏",
         "options": ["font-family", "font-size", "text-align", "margin"],
-        "correct_option_id": 1,
-        "explanation": "Свойство font-size задает размер шрифта текста."
+        "correct_option_id": 1
     },
     {
         "question": "18. Какое CSS-свойство задает отступы между элементами? ⬆️⬇️⬅️➡️",
         "options": ["margin", "padding", "border", "spacing"],
-        "correct_option_id": 0,
-        "explanation": "Свойство margin задает внешние отступы, создавая пространство вокруг элемента."
+        "correct_option_id": 0
     },
     {
         "question": "19. Какое CSS-свойство изменяет стиль ссылок при наведении курсора? 🖱",
-        "options": ["color", "a:hover", "text-decoration", "border"],
-        "correct_option_id": 1,
-        "explanation": "Селектор a:hover позволяет изменять стиль ссылки при наведении курсора мыши."
+
+"options": ["color", "a:hover", "text-decoration", "border"],
+        "correct_option_id": 1
     },
     {
         "question": "20. Какой элемент HTML используется для определения основной части документа? 📝",
         "options": ["<main>", "<section>", "<div>", "<article>"],
-        "correct_option_id": 0,
-        "explanation": "Тег <main> определяет основную часть контента документа, исключая заголовки, футеры и навигацию."
+        "correct_option_id": 0
     }
 ]
-
-
-
-
-
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     keyboard = [
@@ -168,57 +146,38 @@ async def send_question(update: Update, context: ContextTypes.DEFAULT_TYPE) -> N
 async def handle_answer(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     query = update.callback_query
     await query.answer()
+    selected_option = int(query.data)
+    current_question = context.user_data['current_question']
+    question = questions[current_question]
+
+    context.user_data['answers'].append((question['question'], question['options'][selected_option], question['options'][question['correct_option_id']]))
     
-    user_answer = int(query.data)
-    current_question_index = context.user_data['current_question']
-    correct_option_id = questions[current_question_index]['correct_option_id']
-    explanation = questions[current_question_index]['explanation']
-    
-    if user_answer == correct_option_id:
-        context.user_data['score'] += 4
-        context.user_data['answers'].append({
-            'question': questions[current_question_index]['question'],
-            'user_answer': questions[current_question_index]['options'][user_answer],
-            'correct_answer': questions[current_question_index]['options'][correct_option_id],
-            'explanation': explanation
-        })
-    else:
-        context.user_data['answers'].append({
-            'question': questions[current_question_index]['question'],
-            'user_answer': questions[current_question_index]['options'][user_answer],
-            'correct_answer': questions[current_question_index]['options'][correct_option_id],
-            'explanation': explanation
-        })
-    
+    if selected_option == question['correct_option_id']:
+        context.user_data['score'] += 1
+
     context.user_data['current_question'] += 1
-    
+
     if context.user_data['current_question'] < len(questions):
         await send_question(update, context)
     else:
-        score = context.user_data['score']
-        report = f"Поздравляю, {context.user_data['name']}! Ты завершил викторину!\n\nТвой результат: {score:.1f} из 100 баллов.\n\nВот твой отчет:\n"
-        for answer in context.user_data['answers']:
-            report += f"Вопрос: {answer['question']}\nТвой ответ: {answer['user_answer']}\nПравильный ответ: {answer['correct_answer']}\nОбъяснение: {answer['explanation']}\n\n"
-        report += "Спасибо за участие! Если хочешь попробовать еще раз, просто отправь команду /start."
-        
+        score_percentage = (context.user_data['score'] / len(questions)) * 100
+        report = "\n".join(
+            [f"Вопрос: {q[0]}\nВаш ответ: {q[1]}\nПравильный ответ: {q[2]}\n"
+             for q in context.user_data['answers']]
+        )
         await context.bot.send_message(
-            chat_id=update.effective_chat.id,
-            text=report
+            chat_id=update.effective_user.id,
+            text=f"Поздравляю, {context.user_data['name']}! Ты завершил викторину! \nТвой результат: {score_percentage:.1f} из 100 баллов.\n\nВот твой отчет:\n\n{report}\n\nСпасибо за участие! Если хочешь попробовать еще раз, просто отправь команду /start."
         )
 
-async def main() -> None:
+def main() -> None:
     application = Application.builder().token(TOKEN).build()
 
-    application.add_handler(CommandHandler('start', start))
-    application.add_handler(CallbackQueryHandler(button))
-    application.add_handler(CallbackQueryHandler(handle_answer))
+    application.add_handler(CommandHandler("start", start))
+    application.add_handler(CallbackQueryHandler(button, pattern='^(Гарик|Антонина|Сара|Мэри)$'))
+    application.add_handler(CallbackQueryHandler(handle_answer, pattern='^[0-9]+$'))
 
-    # Use existing event loop
-    await application.initialize()
-    await application.start()
-    await application.updater.start_polling()
-    await application.updater.idle()
+    application.run_polling()
 
-if __name__ == '__main__':
-    asyncio.run(main())
-
+if name == 'main':
+    main()
