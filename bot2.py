812de37e7a09575bf3ca315fd0ffdc6286eac76e
romanvolkonidov <romanvolkonidov@gmail.com>
2024-08-17 00:08:46 +0300

    Update bot2.py

--- bot2.py
+++ bot2.py
@@ -1,7 +1,7 @@
 import os
+from telegram import Update, Poll
+from telegram.ext import Application, CommandHandler, PollAnswerHandler, ContextTypes, MessageHandler, filters
 
-from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
-from telegram.ext import Application, CommandHandler, CallbackQueryHandler, ContextTypes, PollAnswerHandler
 
 TOKEN = os.getenv('BOT_TOKEN', '7152066894:AAGkTh2QLFNMSF7Z5dJdfj7IDjcDcDPoKnM')
 
@@ -130,49 +130,37 @@ questions = [
 
 
 
-
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    keyboard = [
-        [InlineKeyboardButton("Гарик", callback_data='Гарик')],
-        [InlineKeyboardButton("Антонина", callback_data='Антонина')],
-        [InlineKeyboardButton("Сара", callback_data='Сара')],
-        [InlineKeyboardButton("Мэри", callback_data='Мэри')]
-    ]
-    reply_markup = InlineKeyboardMarkup(keyboard)
-    await update.message.reply_text('Как вас зовут?', reply_markup=reply_markup)
+    await update.message.reply_text("Привет! Давай познакомимся. Как тебя зовут?")
+    context.user_data['waiting_for_name'] = True
 
-async def button(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    query = update.callback_query
-    await query.answer()
-    context.user_data['name'] = query.data
-    context.user_data['current_question'] = 0
-    context.user_data['score'] = 0
-    await query.edit_message_text(text=f"Приятно познакомиться, {query.data}! Давай начнем викторину.")
-    await send_question(update, context)
+async def handle_name(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    if context.user_data.get('waiting_for_name'):
+        context.user_data['name'] = update.message.text
+        context.user_data['waiting_for_name'] = False
+        context.user_data['score'] = 0
+        context.user_data['current_question'] = 0
+        await update.message.reply_text(f"Приятно познакомиться, {context.user_data['name']}! Давай начнем нашу викторину по HTML и CSS. Удачи тебе!")
+        await send_question(update, context)
 
 async def send_question(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     question = questions[context.user_data['current_question']]
-    options = question['options']
     await context.bot.send_poll(
         chat_id=update.effective_chat.id,
         question=question['question'],
-        options=options,
-        is_anonymous=False,
-        allows_multiple_answers=False,
+        options=question['options'],
+        type=Poll.QUIZ,
+        correct_option_id=question['correct_option_id'],
+        is_anonymous=False
     )
 
 async def handle_answer(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    name = context.user_data.get('name', 'друг')
-    question = questions[context.user_data['current_question']]
-
-    if update.poll_answer.option_ids[0] == question['correct_option_id']:
-        context.user_data['score'] += 1
-
     context.user_data['current_question'] += 1
 
     if context.user_data['current_question'] < len(questions):
         await send_question(update, context)
     else:
+        name = context.user_data.get('name', 'друг')
         score_percentage = (context.user_data['score'] / len(questions)) * 100
         await context.bot.send_message(
             chat_id=update.effective_user.id,
@@ -181,11 +169,9 @@ async def handle_answer(update: Update, context: ContextTypes.DEFAULT_TYPE) -> N
 
 def main() -> None:
     application = Application.builder().token(TOKEN).build()
-
-    application.add_handler(CommandHandler("start", start))
-    application.add_handler(CallbackQueryHandler(button))
+    application.add_handler(CommandHandler('start', start))
+    application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_name))
     application.add_handler(PollAnswerHandler(handle_answer))
-
     application.run_polling()
 
 if __name__ == '__main__':
